756291e018f901e38b86ff24f8a914e179fc8bec
RehanAfridikkk <rehan.gull12345@gmail.com>
2024-01-29 11:06:00 +0500

    tor simple simulation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tor
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Node struct {
+	ID       int
+	Inbound  chan string
+	Outbound chan string
+}
+
+type Network struct {
+	Nodes []*Node
+}
+
+func NewNode(id int) *Node {
+	return &Node{
+		ID:       id,
+		Inbound:  make(chan string),
+		Outbound: make(chan string),
+	}
+}
+
+func NewNetwork(nodeCount int) *Network {
+	network := &Network{}
+	for i := 1; i <= nodeCount; i++ {
+		node := NewNode(i)
+		network.Nodes = append(network.Nodes, node)
+		go network.startNode(node)
+	}
+	return network
+}
+
+func (n *Network) startNode(node *Node) {
+	for {
+		select {
+		case msg := <-node.Inbound:
+			fmt.Printf("Node %d received message: %s\n", node.ID, msg)
+			// Simulate message forwarding (pseudo-anonymity)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+			// Forward the message to a random node
+			destNodeID := rand.Intn(len(n.Nodes)) + 1
+			n.Nodes[destNodeID-1].Inbound <- msg
+		case msg := <-node.Outbound:
+			// Simulate message delivery
+			fmt.Printf("Node %d delivered message: %s\n", node.ID, msg)
+		}
+	}
+}
+
+func (n *Network) sendMessage(srcNodeID int, destNodeID int, msg string) {
+	n.Nodes[srcNodeID-1].Outbound <- msg
+	n.Nodes[destNodeID-1].Inbound <- msg
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	nodeCount := 5
+	network := NewNetwork(nodeCount)
+
+	srcNodeID := rand.Intn(nodeCount) + 1
+	destNodeID := rand.Intn(nodeCount) + 1
+
+	msg := "Hello, anonymous world!"
+
+	network.sendMessage(srcNodeID, destNodeID, msg)
+
+	time.Sleep(time.Second)
+}
